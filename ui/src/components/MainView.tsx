b3dbc979a5c79431060204c5b0a11d665781c3e2
victor.mueller@digitalasset.com <mueller.vpr@gmail.com>
2021-06-14 12:33:31 +0200

    Improve ui to show orders

--- ui/src/components/MainView.tsx
+++ ui/src/components/MainView.tsx
@@ -2,14 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React, { useMemo } from 'react';
-import { Container, Grid, Header, Icon, Segment, Divider, List } from 'semantic-ui-react';
+import { Container, Grid, Header, Icon, Segment, Divider, List, Button, GridColumn } from 'semantic-ui-react';
 import { Party } from '@daml/types';
 // import { User } from '@daml.js/create-daml-app';
 import { useParty, useLedger, useStreamFetchByKeys, useStreamQueries } from '@daml/react';
 // import UserList from './UserList';
 // import PartyListEdit from './PartyListEdit';
 import { OnlineShop } from '@daml.js/create-daml-app'
-import { CreateReservationRequest, ProductDescription, Reservation } from '@daml.js/create-daml-app/lib/OnlineShop';
+import { CreateReservationRequest, Order, OrderRequest, ProductDescription, Reservation } from '@daml.js/create-daml-app/lib/OnlineShop';
 import UserList from './UserList';
 
 // USERS_BEGIN
@@ -35,47 +35,96 @@ const MainView: React.FC = () => {
   const raw_reservations = useStreamQueries(Reservation);
   const reservations = raw_reservations.contracts.map(reservation => reservation.payload)
 
-  const onBuy = async (product: OnlineShop.ProductDescription) => {
+  const raw_orders = useStreamQueries(Order);
+  const orders = raw_orders.contracts.map(order => order.payload)
+
+
+  const onPutInBasket = async (product: OnlineShop.ProductDescription) => {
     await ledger.create(CreateReservationRequest, { customer: username, productName: product.name })
   }
 
+  const onOrder = async () => {
+    await ledger.create(OrderRequest, { customer: username, reservations: reservations.map(res => res.productName) })
+  }
+
+  const productList =
+    <Grid.Column>
+      <Header as='h1' size='medium' color='blue' textAlign='center' style={{ padding: '1ex 0em 0ex 0em' }}>
+        {`Products`}
+      </Header>
+      <Segment>
+        <UserList
+          products={products}
+          onBuy={onPutInBasket}
+        />
+      </Segment>
+    </Grid.Column>
+
+  const basketView =
+    <Grid.Column>
+      <Header as='h1' size='medium' color='blue' textAlign='center' style={{ padding: '1ex 0em 0ex 0em' }}>
+        {`Basket`}
+      </Header>
+      <Segment>
+        <List divided relaxed>
+          {[...reservations].sort((x, y) => x.productName.localeCompare(y.productName)).map(reservation =>
+            <List.Item key={reservation.productName}>
+              <List.Content>
+                <List.Header className='test-select-user-in-network'>{reservation.productName}</List.Header>
+                <List.Content>
+                  <img width="40%" src={productMap.get(reservation.productName)?.imageUrl} />
+                </List.Content>
+                <List.Description>
+
+                </List.Description>
+              </List.Content>
+            </List.Item>
+          )}
+        </List>
+      </Segment>
+      <Button onClick={_ => onOrder()}>"Order now"</Button>
+    </Grid.Column>
+
+  const mkOrderProductList = (order: Order) =>
+      <Segment>
+        <Header>Order status: {order.status}
+        </Header>
+        <List divided relaxed>
+          {[...order.products].sort().map(product =>
+            <List.Item key={product}>
+              <List.Content>
+                <List.Header className='test-select-user-in-network'>{product}</List.Header>
+                <List.Content>
+                  <img width="40%" src={productMap.get(product)?.imageUrl} />
+                </List.Content>
+              </List.Content>
+            </List.Item>
+          )}
+        </List>
+      </Segment>
+
+  const ordersView =
+    <Grid.Column>
+      <Header as='h1' size='medium' color='blue' textAlign='center' style={{ padding: '1ex 0em 0ex 0em' }}>
+        {`Orders`}
+      </Header>
+      {[...orders].map(order => mkOrderProductList(order))}
+      <Button onClick={_ => onOrder()}>"Order now"</Button>
+    </Grid.Column>
+
+
   return (
     <Container>
-      <Grid centered columns={2}>
+      <Grid centered columns={3}>
         <Grid.Row stretched>
           <Grid.Column>
             <Header as='h1' size='huge' color='blue' textAlign='center' style={{ padding: '1ex 0em 0ex 0em' }}>
               {`Welcome, ${username}!`}
             </Header>
-            <Segment>
-              <UserList
-                products={products}
-                onBuy={onBuy}
-              />
-            </Segment>
           </Grid.Column>
-          <Grid.Column>
-            <Header as='h1' size='medium' color='blue' textAlign='center' style={{ padding: '1ex 0em 0ex 0em' }}>
-              {`Basket`}
-            </Header>
-            <Segment>
-              <List divided relaxed>
-                {[...reservations].sort((x, y) => x.productName.localeCompare(y.productName)).map(reservation =>
-                  <List.Item key={reservation.productName}>
-                    <List.Content>
-                      <List.Header className='test-select-user-in-network'>{reservation.productName}</List.Header>
-                      <List.Content>
-                        <img width="40%" src={productMap.get(reservation.productName)?.imageUrl} />
-                      </List.Content>
-                      <List.Description>
-
-                      </List.Description>
-                    </List.Content>
-                  </List.Item>
-                )}
-              </List>
-            </Segment>
-          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          {[productList, basketView, ordersView]}
         </Grid.Row>
       </Grid>
     </Container>
